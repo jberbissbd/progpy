788f5b34dc022a9ae1d46440cf91a77484395d71
jberbissbd <jordi.berbis.11@gmail.com>
2023-03-16 10:29:18 +0100

    Primer esborrany de missatgeria interna

---
+++ missatgeria.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+
+
+@dataclass(repr=False)
+class criteri:
+    id: int
+    num: int
+    descripcio: str
+
+
+@dataclass(repr=False)
+class competencia:
+    id: int
+    num: int
+    descripcio: str
+
+
+@dataclass(repr=False)
+class saber:
+    id: int
+    descripcio: str
+
+
+@dataclass(repr=False)
+class materia(dataclass):
+    id_materia: int
+    nom: str
+    curs: int
+    criteris: list
+    sabers: list
+    competencies: list
