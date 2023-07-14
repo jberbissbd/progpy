047635834242aafacb61689c5007476d5e650241
jberbissbd <jordi.berbis.11@gmail.com>
2023-07-14 07:18:21 +0200

    Eliminats errors sense sentit

--- src/main.py
+++ src/main.py
@@ -1,22 +1,9 @@
-# from moduls import bbdd as base
-import sys
-# import os
-from moduls import bbdd 
-from bbdd import LectorMateriesCompletes
-
-# sys.path.append("../")
-print(sys.path)
-
-encoding = 'utf-8'
-
-
-
-
-encoding = 'utf-8'
+# encoding = 'utf-8'
+from moduls.bbdd import LectorMateriesCompletes  # pylint: disable=import-error
 
 
 b = LectorMateriesCompletes(0)
-b.taula= "taula_falsa"
+b.taula = "taula_falsa"
 c = b.combinar_info_materies()
 
 print(c)
