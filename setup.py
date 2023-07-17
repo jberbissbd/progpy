047c46eb06deb11a6d45d16b8a96d11f43d9105a
jberbissbd <jordi.berbis.11@gmail.com>
2023-07-17 10:53:25 +0200

    Modificant estructura del projecte i afegint arxius de configuració.

---
+++ setup.py
@@ -0,0 +1,5 @@
+"""Genèric setup.py"""
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup()
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,7 @@
+"""Configuració inicial per als tests"""
+
 import sys
 import os
-from os.path import dirname,join
-sys.path.append(os.path.abspath(join(dirname((dirname(__file__))),"src/moduls")))
-print(sys.path)
+from os.path import dirname, join
+sys.path.append(os.path.abspath(join(dirname((dirname(__file__))),
+                                     "src/moduls")))
