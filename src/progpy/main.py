c48e0ac8ef34ffe6dbc633a355a6eaa78bd819b2
jberbissbd <jordi.berbis.11@gmail.com>
2023-07-24 18:37:28 +0200

    Armonització prèvia a merge

--- src/progpy/main.py
+++ src/progpy/main.py
@@ -1,10 +1,7 @@
 # encoding = 'utf-8'
-from progpy.moduls.bbdd import LectorMateriesCompletes  # pylint: disable=import-error
+from progpy.moduls.bbdd import LectorMateriesCompletes # pylint: disable=import-error
 
 
 b = LectorMateriesCompletes(1)
-b.taula = "materies_completes"
-print(b.ruta_arxiu_bbdd)
-c = b.combinar_info_materies()
-
-print(c)
+g = b.obtenir_materies()
+print(g)
